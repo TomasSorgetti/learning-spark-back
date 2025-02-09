51a1ef79d325cbc6fb2a9dfed66e572724c24e54
TomasSorgetti <tomassorgetti456@gmail.com>
2025-02-09 00:10:33 -0300

    feat: admin guard created

---
+++ src/infrastructure/middlewares/isAdminGuard.ts
@@ -0,0 +1,47 @@
+import { Request, Response, NextFunction } from "express";
+import { UnauthorizedError } from "../../shared/utils/app-errors";
+import { UserService } from "../../application/services/UserService";
+
+interface CustomRequest extends Request {
+  userId?: string;
+  sessionId?: string;
+}
+const userService = new UserService();
+
+const isAdminGuard = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userId = req.userId;
+    if (!userId) {
+      throw new UnauthorizedError("No user id provided");
+    }
+
+    const user = await userService.getUserById(userId);
+    if (!user) {
+      throw new UnauthorizedError("User not found");
+    }
+
+    if (user && user.deleted) {
+      throw new UnauthorizedError("User deleted");
+    }
+
+    if (user && !user.validated) {
+      throw new UnauthorizedError("User not validated");
+    }
+
+    const isAdmin = user.roles.some((role: any) => role.name === "admin");
+
+    if (!isAdmin) {
+      throw new UnauthorizedError("User is not admin");
+    }
+
+    next();
+  } catch (error: any) {
+    next(error);
+  }
+};
+
+export default isAdminGuard;
